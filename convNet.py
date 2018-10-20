6a02eaa556d6bc48ec01e77d691bbc3b6b378a33
Andrés Felipe Zapata Palacio <afzp99@gmail.com>
2018-10-20 18:24:24 -0500

    New modifications

--- convNet.py
+++ convNet.py
@@ -8,47 +8,91 @@ TOTAL=560
 TESTS=10
 TRAIN=10
 PERIODS=TRAIN
+FOLDER="/home/afzp99/Documentos/2. DATASET PLACAS/3. Caracteres de Placas/"
 
-def conv_relu():
-    pass
+def conv_relu(inputs, filters, k_size, stride, padding, scope_name):
+    with tf.variable_scope(scope_name, reuse=tf.AUTO_REUSE) as scope:
+        in_channels = inputs.shape[-1]
+        kernel = tf.get_variable('kernel', 
+                                 [k_size, k_size, in_channels, filters], 
+                                 initializer=tf.truncated_normal_initializer())
+        biases = tf.get_variable('biases', 
+                                 [filters],
+                                 initializer=tf.random_normal_initializer())
+        conv = tf.nn.conv2d(inputs, kernel, strides=[1, stride, stride, 1], padding=padding)
+    return tf.nn.relu(conv + biases, name=scope.name)
 
-def maxpool():
-    pass
+def maxpool(inputs, ksize, stride, padding='VALID', scope_name='pool'):
+    with tf.variable_scope(scope_name, reuse=tf.AUTO_REUSE) as scope:
+        pool = tf.nn.max_pool(inputs, 
+                              ksize=[1, ksize, ksize, 1], 
+                              strides=[1, stride, stride, 1],
+                              padding=padding)
+    return pool
 
-def fully_connected():
-    pass
+def fully_connected(inputs, out_dim, scope_name='fc'):
+    with tf.variable_scope(scope_name, reuse=tf.AUTO_REUSE) as scope:
+        in_dim = inputs.shape[-1]
+        w = tf.get_variable('weights', [in_dim, out_dim],
+                            initializer=tf.truncated_normal_initializer())
+        b = tf.get_variable('biases', [out_dim],
+                            initializer=tf.constant_initializer(0.0))
+        out = tf.matmul(inputs, w) + b
+    return out
 
 class ConvNetwork():
     def __init__(self):
         self.lr = 0.001
-        self.batch_size = 128
-        #self.keep_prob = tf.constant(0.75)
+        self.batch_size = 10
+        self.keep_prob = tf.constant(0.75)
         self.globalstep = tf.Variable(0, dtype=tf.int32, 
                                 trainable=False, name='global_step')
         self.n_classes = 10+26
         self.skip_step = 20
         self.n_test = TESTS
         self.training = True
-        # Get data
+        
+    def get_data(self):
         with tf.name_scope('data'):
-            train_data, test_data = tools.get_dataset(self.batch_size)
+            folder = FOLDER
+            train_data, test_data = tools.get_dataset(folder,TRAIN,TESTS,self.batch_size)
             iterator = tf.data.Iterator.from_structure(train_data.output_types,
                                                        train_data.output_shapes)
             img, self.label = iterator.get_next()
             self.img = tf.reshape(img, shape=[-1, 28, 28, 1])
             self.train_init = iterator.make_initializer(train_data)  # initializer for train_data
             self.test_init = iterator.make_initializer(test_data)    # initializer for train_data
-
-
-    def _loss(self):
+            
+    def inference(self):
+        conv1 = conv_relu(inputs=self.img,
+                        filters=32,
+                        k_size=5,
+                        stride=1,
+                        padding='SAME',
+                        scope_name='conv1')
+        pool1 = maxpool(conv1, 2, 2, 'VALID', 'pool1')
+        conv2 = conv_relu(inputs=pool1,
+                        filters=64,
+                        k_size=5,
+                        stride=1,
+                        padding='SAME',
+                        scope_name='conv2')
+        pool2 = maxpool(conv2, 2, 2, 'VALID', 'pool2')
+        feature_dim = pool2.shape[1] * pool2.shape[2] * pool2.shape[3]
+        pool2 = tf.reshape(pool2, [-1, feature_dim])
+        fc = fully_connected(pool2, 1024, 'fc')
+        dropout = tf.nn.dropout(tf.nn.relu(fc), self.keep_prob, name='relu_dropout')
+        self.logits = fully_connected(dropout, self.n_classes, 'logits')
+            
+    def loss(self):
         with tf.name_scope('loss'):
             entropy = tf.nn.softmax_cross_entropy_with_logits(labels=self.label, logits=self.logits)
             self.loss = tf.reduce_mean(entropy, name='loss')
 
-    def _optimize(self):
+    def optimize(self):
         self.opt = tf.train.AdamOptimizer(self.lr).minimize(self.loss,global_step=self.globalstep)
 
-    def _create_summary(self):
+    def create_summary(self):
         with tf.name_scope('summaries'):
             tf.summary.scalar('loss', self.loss)
             tf.summary.scalar('accuracy', self.accuracy)
@@ -56,7 +100,7 @@ class ConvNetwork():
             self.summary_op = tf.summary.merge_all()
 
     # N of Right predictions
-    def _eval_model(self):
+    def eval_model(self):
         with tf.name_scope('predict'):
             preds = tf.nn.softmax(self.logits)
             correct_preds = tf.equal(tf.argmax(preds, 1), tf.argmax(self.label, 1))
@@ -67,20 +111,46 @@ class ConvNetwork():
         self.inference()
         self.loss()
         self.optimize()
-        self.eval()
+        self.eval_model()
         self.create_summary()
 
-    def _train_epoch(self, sess, saver, init, writer, epoch, step):
-        print(sess)
-        print(saver)
-        print(init)
-        print(writer)
-        print(epoch)
-        print(step)
-        pass
+    def train_epoch(self, sess, saver, init, writer, epoch, step):
+        start_time = time.time()
+        sess.run(init) 
+        self.training = True
+        total_loss = 0
+        n_batches = 0
+        try:
+            while True:
+                _, l, summaries = sess.run([self.opt, self.loss, self.summary_op])
+                writer.add_summary(summaries, global_step=step)
+                if (step + 1) % self.skip_step == 0:
+                    print('Loss at step {0}: {1}'.format(step, l))
+                step += 1
+                total_loss += l
+                n_batches += 1
+        except tf.errors.OutOfRangeError:
+            pass
+        saver.save(sess, 'checkpoints/convnet_mnist/mnist-convnet', step)
+        print('Average loss at epoch {0}: {1}'.format(epoch, total_loss/n_batches))
+        print('Took: {0} seconds'.format(time.time() - start_time))
+        return step
 
-    def _eval_once(self):
-        pass
+    def eval_once(self, sess, init, writer, epoch, step):
+        start_time = time.time()
+        sess.run(init)
+        self.training = False
+        total_correct_preds = 0
+        try:
+            while True:
+                accuracy_batch, summaries = sess.run([self.accuracy, self.summary_op])
+                writer.add_summary(summaries, global_step=step)
+                total_correct_preds += accuracy_batch
+        except tf.errors.OutOfRangeError:
+            pass
+
+        print('Accuracy at epoch {0}: {1} '.format(epoch, total_correct_preds/self.n_test))
+        print('Took: {0} seconds'.format(time.time() - start_time))
     
     def train(self, periods,checkpoint_frequency):
         tools.makedir('checkpoints/convnet')
@@ -96,10 +166,13 @@ class ConvNetwork():
                 step = self.train_epoch(sess, saver, self.train_init, writer, epoch, step)
                 self.eval_once(sess, self.test_init, writer, epoch, step)
                 if (epoch + 1) % checkpoint_frequency == 0:
-                    saver.save(sess, 'checkpoints/convnet/checkpoint', index)
+                    saver.save(sess, 'checkpoints/convnet/checkpoint', epoch)
         writer.close()
 
+    def predict(self,img):
+        self.evaluate()
+        
 if __name__ == '__main__':
     model = ConvNetwork()
     model.build_graph()
-    model.train(periods=PERIODS)
+    model.train(PERIODS,1)
--- tools.py
+++ tools.py
@@ -4,6 +4,9 @@ import cv2
 import pandas as pd
 from matplotlib import pyplot as plt
 import numpy as np
+import tensorflow as tf
+import sklearn
+from sklearn import preprocessing
 
 def showImage(image):
     plt.imshow(image, cmap='gray')
@@ -60,49 +63,70 @@ def get_dataset(folder):
 def loadImage(folder,file):
     #file = "9-0-coplate30.png"
     img = cv2.imread(folder+"/"+file,0)
-    image = np.asarray(img)
+    img = cv2.resize(img,(28,28))
+    image = np.asarray(img, dtype=np.float32)
+    image = sklearn.preprocessing.normalize(image)
+    image[image >= 0.1] = 1
     label_index = file.split("-")[0]
-    print(label_index, end="-")
+    #print(image.shape, end="****")
+    #plt.imshow(image, cmap='gray')
+    #plt.title(label_index)
+    #plt.show()    
     if (label_index.isdigit()):
         label_index = ord(label_index) - ord('0')
     else:
         label_index = ord(label_index) - 87
     label = np.zeros(36)
     label[label_index] = 1
-    print(label)
-    plt.imshow(image, cmap='gray')
-    plt.title(label)
+#    print(label)
+#    plt.imshow(image, cmap='gray')
+#    plt.title(label)
     #plt.show()
     return image, label
 
 def full_dataset(folder):
     full_dataset = pd.DataFrame()
-    folder = "/home/afzp99/Documentos/2. DATASET PLACAS/3. Caracteres de Placas/"
+#    folder = "/home/afzp99/Documentos/2. DATASET PLACAS/3. Caracteres de Placas/"
     folder = folder[:-1]
     files = os.listdir(folder)
     random.shuffle(files)
     #print(files)
     number_files = len(files) 
-    print(number_files)
+#    print(number_files)
     images = []
     labels = []
     for file in files:
         image,label = loadImage(folder, file)
+        #print("image shape")
+        #print(image.shape)
+        #print("label shape")
+        #print(label.shape)
         images.append(image)
         labels.append(label)
     images = np.array(images)
     labels = np.array(labels)
-    print("images type after")
-    print(type(images[0]))
-    print("labels type after")
-    print(type(labels[0]))
-    
-#    print(images)
-#    print(labels)
-    return full_dataset
+#    print("images type after")
+#    print(type(images[0]))
+#    print("labels type after")
+#    print(type(labels[0]))
+    #    print(images)
+    #    print(labels)
+    return images,labels
 
-def loadDataset():
-    pass
+def get_dataset(folder, train_size, test_size, batch_size):
+    images, labels = full_dataset(folder)
+    train=(images,labels)
+    print(train[0][0].shape)
+    print(train[1].shape)
+    test=(images,labels)
+    train_data = tf.data.Dataset.from_tensor_slices(train)
+    train_data = train_data.batch(batch_size)
+    test_data = tf.data.Dataset.from_tensor_slices(test)
+    test_data = test_data.batch(batch_size)
+    return train_data, test_data
+
+#    return (images,labels),(images,labels)
+
 #loadImages()
 #makedir("/tmp/folders/anidadas")
-full_dataset("")
+#full_dataset("")
